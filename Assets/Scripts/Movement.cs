5e1904fe07ab4ea5a38a098f3ae86e261cba8441
Alperen Erol <alpereneroltr@gmail.com>
2024-11-21 19:29:32 +0300

    thrust-rotation

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,16 +4,41 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
+    [SerializeField] float thrustStr = 10f;
+    [SerializeField] float rotationStr = 10f;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
-    private void Update()
+    private void FixedUpdate()
+    {
+        ThrustHandle();
+        RotationHandle();
+    }
+
+    private void ThrustHandle()
     {
         if (thrust.IsPressed())
         {
-            Debug.Log("heyo");
+            rb.AddRelativeForce(Vector3.up * thrustStr * Time.fixedDeltaTime);
         }
+
+    }
+    private void RotationHandle()
+    {
+        rb.freezeRotation = true;
+        float rotationInput = rotation.ReadValue<float>() * -1;
+        transform.Rotate(Vector3.forward * rotationInput * rotationStr * Time.fixedDeltaTime);
+        rb.freezeRotation = false;
+
     }
 }
